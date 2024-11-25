166ac7e896a3fb6d87379b489c6f90ecf954500a
jimefle <jimefle@icloud.com>
2024-11-25 17:38:12 -0300

    modif path photo-profile

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -6,7 +6,7 @@ function Aboutme() {
               <h2 className="px-2 text-2xl font-bold" id='aboutme'>Sobre mí</h2>
             </div>
             <article className="flex gap-5">
-              <img src=".\src\image\photo-profile.jpg" alt="Foto de perfil" className="rounded-full h-48 object-contain w-48" />
+              <img src="/photo-profile.jpg" alt="Foto de perfil" className="rounded-full h-48 object-contain w-48" />
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam efficitur risus ac justo faucibus, 
                 et porta turpis tempor. Ut at sagittis metus. Nunc congue nunc ut volutpat molestie. Aenean vitae eros 
                 sed nisl elementum mattis vel a massa. Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
