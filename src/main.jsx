c8dededcbd210ec0e47fc1a0b18e2374df9a8861
jimefle <jimefle@icloud.com>
2024-11-25 11:23:57 -0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import App from './App.jsx'
 import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
