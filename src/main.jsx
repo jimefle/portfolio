be3fec0869a8d7d017d7d12aec703302c5b1ca5d
jimefle <jimefle@icloud.com>
2024-11-25 11:10:08 -0300

    ts to js

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
