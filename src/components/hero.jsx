974d4d7601a099fa6a09d88e4cddedae3a977b32
jimefle <jimefle@icloud.com>
2024-11-25 11:45:10 -0300

    importar tailwind

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,8 +1,8 @@
 function Hero(){
     return(
-        <section className='hero'>
-            <h1 className='welcome'>Hola! Soy Jimena</h1>
-            <p className='welcome-info'>Soy estudiante de 5to año de <strong>Ingeniería en Sistemas de Información</strong>. Mi objetivo es seguir 
+        <section className="text-center p-10">
+            <h1 className="text-4xl font-bold text-blue-600">Hola! Soy Jimena</h1>
+            <p className="text-gray-500">Soy estudiante de 5to año de <strong>Ingeniería en Sistemas de Información</strong>. Mi objetivo es seguir 
                 aprendiendo y creciendo profesionalmente, por lo que estoy abierta a
                 nuevos desafíos que me permitan aplicar mis conocimientos teóricos y prácticos y ampliar mis
                 habilidades tanto técnicas como interpersonales.</p>
